915824ad4f94ceea01a243f1831e14be9afd5912
Aleksey <aleksey91scorp@bk.ru>
2020-06-08 17:08:42 +0300

    get/set for class dom added

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -2,7 +2,7 @@ import {$} from '@core/dom'
 
 export class Excel {
   constructor(selector, options) {
-    this.$el = document.querySelector(selector)
+    this.$el = $(selector)
     this.components = options.components || []
   }
 
@@ -12,7 +12,7 @@ export class Excel {
     this.components.forEach(Component => {
       const $el = $.create('div', Component.className)
       const component = new Component($el)
-      $el.innerHTML = component.toHTML()
+      $el.html(component.toHTML())
       $root.append($el)
     })
 
--- src/core/dom.js
+++ src/core/dom.js
@@ -1,8 +1,45 @@
 class Dom {
+  constructor(selector) {
+    this.$el = typeof selector === 'string' ?
+      document.querySelector(selector) :
+      selector
+  }
+
+  html(html) {
+    if (typeof html === 'string') {
+      this.$el.innerHTML = html
+      return this
+    }
+    return this.$el.outerHTML.trim()
+  }
+
+  clear() {
+    this.html('')
+    return this
+  }
+
+  on() {
+
+  }
+
+  append(node) {
+    if (node instanceof Dom) {
+      node = node.$el
+    }
+
+    if (Element.prototype.append) {
+      this.$el.append(node)
+    } else {
+      this.$el.appendChild(node)
+    }
+
+    return this
+  }
 }
 
-export function $() {
-  return new Dom()
+// event.target
+export function $(selector) {
+  return new Dom(selector)
 }
 
 $.create = (tagName, classes = '') => {
@@ -10,5 +47,5 @@ $.create = (tagName, classes = '') => {
   if (classes) {
     el.classList.add(classes)
   }
-  return el
+  return $(el)
 }
