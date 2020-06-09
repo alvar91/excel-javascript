fbf4a0b4bee719e39c6a31f923a21af675b0736d
Aleksey <aleksey91scorp@bk.ru>
2020-06-09 21:40:09 +0300

    headers of table added

---
+++ src/components/table/table.template.js
@@ -0,0 +1,48 @@
+const CODES = {
+  A: 65,
+  Z: 90
+}
+
+// function createCell() {
+//   return `
+//     <div class="cell" contenteditable>B2</div>
+//   `
+// }
+
+function toColumn(col) {
+  return `
+    <div class="column">${col}</div>
+  `
+}
+
+function createRow(content) {
+  return `
+    <div class="row">
+      <div class="row-info"></div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
+}
+
+function toChar(_, index) {
+  return String.fromCharCode(CODES.A + index)
+}
+
+export function createTable(rowsCount = 15) {
+  const colsCount = CODES.Z - CODES.A + 1
+  const rows = []
+
+  const cols = new Array(colsCount)
+      .fill('')
+      .map(toChar)
+      .map(toColumn)
+      .join('')
+
+  rows.push(createRow(cols))
+
+  for (let i = 0; i < rowsCount; i++) {
+    rows.push(createRow())
+  }
+
+  return rows.join('')
+}
