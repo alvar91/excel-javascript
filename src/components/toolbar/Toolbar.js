3b09a93569904884b4ca8494f6e1ec326bacea17
Aleksey <aleksey91scorp@bk.ru>
2020-06-13 21:53:08 +0300

    state of excel component added

--- src/components/toolbar/Toolbar.js
+++ src/components/toolbar/Toolbar.js
@@ -1,8 +1,8 @@
-import {ExcelComponent} from '@core/ExcelComponent'
 import {createToolbar} from '@/components/toolbar/toolbar.template'
 import {$} from '@core/dom'
+import {ExcelStateComponent} from '@core/ExcelStateComponent'
 
-export class Toolbar extends ExcelComponent {
+export class Toolbar extends ExcelStateComponent {
   static className = 'excel__toolbar'
 
   constructor($root, options) {
@@ -13,14 +13,31 @@ export class Toolbar extends ExcelComponent {
     })
   }
 
+  prepare() {
+    const initialState = {
+      textAlign: 'left',
+      fontWeight: 'normal',
+      textDecoration: 'none',
+      fontStyle: 'normal'
+    }
+    this.initState(initialState)
+  }
+
+  get template() {
+    return createToolbar(this.state)
+  }
+
   toHTML() {
-    return createToolbar()
+    return this.template
   }
 
   onClick(event) {
     const $target = $(event.target)
     if ($target.data.type === 'button') {
-      console.log($target.data.value)
+      const value = JSON.parse($target.data.value)
+      const key = Object.keys(value)[0]
+      this.setState({[key]: value[key]})
+      console.log(this.state)
     }
   }
 }
--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -16,7 +16,7 @@ function toButton(button) {
   `
 }
 
-export function createToolbar() {
+export function createToolbar(state) {
   const buttons = [
     {
       icon: 'format_align_left',
@@ -35,8 +35,8 @@ export function createToolbar() {
     },
     {
       icon: 'format_bold',
-      active: false,
-      value: {fontWeight: 'bold'}
+      active: state['fontWeight'] === 'bold',
+      value: {fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold'}
     },
     {
       icon: 'format_italic',
