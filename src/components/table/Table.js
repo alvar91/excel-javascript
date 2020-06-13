d89153e97b99d3c2d60e803a92c177c05ffb9f4e
Aleksey <aleksey91scorp@bk.ru>
2020-06-13 20:09:24 +0300

    render of state added

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -18,7 +18,7 @@ export class Table extends ExcelComponent {
   }
 
   toHTML() {
-    return createTable(20)
+    return createTable(20, this.store.getState())
   }
 
   prepare() {
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,14 +3,15 @@ const CODES = {
   Z: 90
 }
 
-// function toCell(row, col) {
-//   return `
-//     <div class="cell" contenteditable data-col="${col}"></div>
-//   `
-// }
+const DEFAULT_WIDTH = 120
 
-function toCell(row) {
+function getWidth(state, index) {
+  return (state[index] || DEFAULT_WIDTH) + 'px'
+}
+
+function toCell(state, row) {
   return function(_, col) {
+    const width = getWidth(state.colState, col)
     return `
       <div 
         class="cell" 
@@ -18,14 +19,20 @@ function toCell(row) {
         data-col="${col}"
         data-type="cell"
         data-id="${row}:${col}"
+        style="width: ${width}"
       ></div>
     `
   }
 }
 
-function toColumn(col, index) {
+function toColumn({col, index, width}) {
   return `
-    <div class="column" data-type="resizable" data-col="${index}">
+    <div 
+      class="column" 
+      data-type="resizable" 
+      data-col="${index}" 
+      style="width: ${width}"
+    >
       ${col}
       <div class="col-resize" data-resize="col"></div>
     </div>
@@ -49,13 +56,22 @@ function toChar(_, index) {
   return String.fromCharCode(CODES.A + index)
 }
 
-export function createTable(rowsCount = 15) {
+function withWidthFrom(state) {
+  return function(col, index) {
+    return {
+      col, index, width: getWidth(state.colState, index)
+    }
+  }
+}
+
+export function createTable(rowsCount = 15, state = {}) {
   const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
   const rows = []
 
   const cols = new Array(colsCount)
       .fill('')
       .map(toChar)
+      .map(withWidthFrom(state))
       .map(toColumn)
       .join('')
 
@@ -64,8 +80,7 @@ export function createTable(rowsCount = 15) {
   for (let row = 0; row < rowsCount; row++) {
     const cells = new Array(colsCount)
         .fill('')
-        // .map((_, col) => toCell(row, col))
-        .map(toCell(row))
+        .map(toCell(state, row))
         .join('')
 
     rows.push(createRow(row + 1, cells))
