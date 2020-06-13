95a98d604ad0bdbce9878f94ea229715d9ce8a51
Aleksey <aleksey91scorp@bk.ru>
2020-06-13 19:34:30 +0300

    state of columns added

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {rootReducer} from '@/redux/rootReducer'
 import './scss/index.scss'
 
 const store = createStore(rootReducer, {
-  tableTitle: 'My Table excel'
+  colState: {}
 })
 
 const excel = new Excel('#app', {
