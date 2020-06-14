dca9923d8d566eb17ba387590e755cda93d08ec1
Aleksey <aleksey91scorp@bk.ru>
2020-06-14 21:35:22 +0300

    dashboard realized

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,43 +1,26 @@
 import {Page} from '@core/Page'
 import {$} from '@core/dom'
+import {createRecordsTable} from './dashboard.functions'
 
 export class DashboardPage extends Page {
   getRoot() {
+    const now = Date.now().toString()
     return $.create('div', 'db').html(`
       <div class="db__header">
-      <h1>Excel Dashboard</h1>
-    </div>
-
-    <div class="db__new">
-      <div class="db__view">
-        <a href="#" class="db__create">
-          Новая <br /> Таблица
-        </a>
-      </div>
-    </div>
-
-    <div class="db__table db__view">
-
-      <div class="db__list-header">
-        <span>Название</span>
-        <span>Дата открытия</span>
+        <h1>Excel. Панель Управления</h1>
       </div>
 
-      <ul class="db__list">
+      <div class="db__new">
+        <div class="db__view">
+          <a href="#excel/${now}" class="db__create">
+            Новая <br /> Таблица
+          </a>
+        </div>
+      </div>
 
-        <li class="db__record">
-          <a href="#">Таблица номер 1</a>
-          <strong>12.06.2020</strong>
-        </li>
-
-        <li class="db__record">
-          <a href="#">Таблица номер 2</a>
-          <strong>12.06.2020</strong>
-        </li>
-
-      </ul>
-
-    </div>
+      <div class="db__table db__view">
+        ${createRecordsTable()}
+      </div>
     `)
   }
 }
---
+++ src/pages/dashboard.functions.js
@@ -0,0 +1,42 @@
+function toHTML() {
+  return `
+    <li class="db__record">
+      <a href="#">Таблица номер 1</a>
+      <strong>12.06.2020</strong>
+    </li>
+  `
+}
+
+// excel:12312
+// excel:1232112
+function getAllKeys() {
+  const keys = []
+  for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i)
+    if (!key.includes('excel')) {
+      continue
+    }
+    keys.push(key)
+  }
+  return keys
+}
+
+export function createRecordsTable() {
+  const keys = getAllKeys()
+
+  if (!keys.length) {
+    return `<p>Вы пока не создали ни одной таблицы</p>`
+  }
+
+  return `
+    <div class="db__list-header">
+      <span>Название</span>
+      <span>Дата открытия</span>
+    </div>
+
+    <ul class="db__list">
+      ${keys.map(toHTML).join('')}
+    </ul>
+  `
+}
+
