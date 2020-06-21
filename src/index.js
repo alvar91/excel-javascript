5967b2d93b596d1697064fbab195a60adb0bc247
Aleksey <aleksey91scorp@bk.ru>
2020-06-21 18:27:08 +0300

    Updates

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {Router} from '@core/routes/Router'
-import {DashboardPage} from '@/pages/DashboardPage'
-import {ExcelPage} from '@/pages/ExcelPage'
-import './scss/index.scss'
-
-new Router('#app', {
-  dashboard: DashboardPage,
-  excel: ExcelPage
-})
